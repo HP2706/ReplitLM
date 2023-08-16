2248d0c9dcdacf2a7173be5b065dca6e02d59265
HP2706 <hprjdk@gmail.com>
2023-08-16 11:22:35 +0200

    .

--- replitLM_spec/modeling_mpt.py
+++ replitLM_spec/modeling_mpt.py
@@ -249,9 +249,9 @@ class MPTModel(MPTPreTrainedModel):
                     weight_factor = 0.
                 biolinear = linears[i]
                 print("layer type", type(biolinear), "is bias none?", biolinear.linear.bias is None)
+                print("biolinear.linear.weight", biolinear.linear.weight)
                 dist = torch.abs(biolinear.out_coordinates.unsqueeze(dim=1) - biolinear.in_coordinates.unsqueeze(dim=0))
                 print("device of torch.mean(torch.abs(biolinear.linear.weight)", torch.mean(torch.abs(biolinear.linear.weight)).device)
-                print("device of (weight_factor*dist+self.l0)", (weight_factor*dist+self.l0).device)
                 cc += torch.mean(torch.abs(biolinear.linear.weight)*(weight_factor*dist+self.l0).to('cuda'))
                 if bias_penalize == True:
                     cc += torch.mean(torch.abs(biolinear.linear.bias)*(self.l0))
